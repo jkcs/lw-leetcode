52763522ac64da6bbf3f083a7878a910fda66388
liuwei <liuwei@woaizuji.com>
2020-05-12 17:24:24 +0800

    解决问题1

---
+++ question/1.js
@@ -0,0 +1,33 @@
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，
+ * 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 
+ * 
+ * 示例:
+ * 
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        let diff = target - num
+        for (let j = 0; j < nums.length; j++) {
+            if (i === j) continue
+            if (nums[j] === diff) {
+                return [i, j]
+            }
+        }
+    }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
